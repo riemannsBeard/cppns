f09f5ed19fdd8792c57393b918a3f9baffed1d4b
Joshua Christopher <jcc242@nau.edu>
2013-11-04 21:38:43 -0700

    Minor work on step 2, will not build because errors in step2

--- step2/nonlinear-1d.cpp
+++ step2/nonlinear-1d.cpp
@@ -7,26 +7,28 @@ using namespace std;
 
 int main()
 {
-    double nx = 41;
+    int nx = 41;
     double dx = 2/(nx-1);
-    double nt = 20;
+    int nt = 20;
     double dt = .025;
 
-    Eigen::MatrixXd u(2,7); // Creates a matrix with a for for every point and a column for every timestep. This will make plotting a lot easier than step1 (hopefully...)
+    Eigen::MatrixXd u(nx,nt+1); // Creates a matrix with a column for every point and a column for every timestep. This will make plotting a lot easier than step1 (hopefully...)
+    // The more I think about this, the more I think this is a bad idea. It is nonsensical to store 1d data in a 2d matrix
 
-    u.setOnes(5,7);
+    u.setOnes(nx,nt+1);
+
+    Eigen::VectorXd x(nx); // This will set our x values to the first column in the matrix
+    u.col(0) = x.setLinSpaced(nx,0,2);
+
+    double s = (1/dx+1)-(.5/dx); // The number of rows for starting position.
+
+    Eigen::MatrixXd sb(s,nt); // initial conditions block matrix
+
+    sb.setConstant(s,nt,2);
+
+    u.block(.5/dx,1,s,nt) = sb;
+
     cout << u << endl;
-    cout << u.block<2,2>(1,1) << endl;
-
-    Eigen::MatrixXd m(2,2);
-    m << 2,4,
-         5,9;
-
-     u.block<2,2>(1,1) = m;
-
-    std::cout << u.rows() << std::endl;
-
-    std::cout << u << std::endl;
 
     return 0;
 }

5f6b2f6a1f989de872405357d3782298a26b6dcc
Joshua Christopher <jcc242@nau.edu>
2013-12-01 22:36:12 -0700

    Started working on step 3, does not compile, need to fix string/char concat

---
+++ src/diffusion1d.cpp
@@ -0,0 +1,70 @@
+#include "nonlin1d.h"
+
+using namespace std;
+
+void diffusion1d()
+{
+    const int nx = 41;
+    const double dx = 2/(double (nx)-1);
+    const int nt = 20;
+    const double nu = .3;
+    const double sigma = 2;
+    const double dt = (sigma*pow(dx,2.0))/nu;
+
+    Eigen::VectorXd u(nx);
+
+    u.setOnes(nx); // Set ones everywhere
+
+    //Eigen::VectorXd x(nx); // This will be our x-values
+    //x.setLinSpaced(nx,0,2); //linearly spaced values between 0 and 2
+
+    int r = (1/dx+1)-(.5/dx); // The number of rows for starting position.
+    // r should always be int, right? ... right? maybe should add a check...
+
+    Eigen::VectorXd ic(r); // initial conditions block matrix
+    // Needs to be only nt+1, because we don't want to modify our newly created x column
+
+    ic.setConstant(r,2); // nt+1 again for same reason as above
+
+    u.segment(.5/dx,r) = ic; // Substiutes the sb matrix into the overall matrix.
+
+    Eigen::VectorXd un(nx);
+
+    char dir[80] = "step3/";
+
+    mkdir(dir,0755); //Make a folder to store results in
+
+    for (int j=0;j<nt;j++) // From column 2 (Eigen is 0-indexed, so really column 3!) to end (of matrix, nt+2)
+    {
+        un << u;
+        char step = char(dt*(j+1));
+        cout << step << endl;
+
+        for (int k=1;k<nx;k++) // From row 1
+        {
+            u(k) = un(k) + nu*dt/pow(dx,2)*(un(k+1)-2*un(k)+un(k-1));
+            ofstream writedata(strcat(strcat(dir,step),".dat"), ios::out | ios::trunc);
+            writedata << u << endl;
+            writedata.close();
+        }
+    }
+
+
+    mglGraph gr; // Start mgl graph object that holds all graph stuff
+
+    mglData x;
+    x.Fill(0,2,'x');
+    mglData y(strcat(strcat(dir,step),".dat")); // Load one set of data
+    //mglData xdat=y.SubData(0), ydat=y.SubData(21); // Deprecated?
+
+    gr.SetOrigin(0,0,0);
+    gr.Title("Non-Linear 1 Dimension");
+    gr.Box(); // Bounding box
+    gr.AddLegend("Final Condition","r*");
+    gr.Legend(); // Add legend to plot
+    gr.SetRanges(0,2,0,3);
+    gr.Axis(); // Creates ticks, I think
+    gr.Plot(x,y,"r*"); // Same as previous line
+    gr.WriteGIF("step2/nonlin1d.gif");
+
+}
---
+++ src/diffusion1d.h
@@ -0,0 +1,13 @@
+#ifndef NONLINEAR1D_H
+#define NONLINEAR1D_H
+
+#include <iostream>
+#include <Eigen/Dense>
+#include <mgl2/mgl.h>
+#include <fstream>
+#include <sys/stat.h>
+#include <cmath>
+
+void diffusion1d();
+
+#endif // NONLINEAR1D_H

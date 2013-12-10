46a9e397f7c98afb98c386fa386336a2100fede0
Joshua Christopher <jcc242@nau.edu>
2013-12-09 22:41:27 -0700

    Step 3 works... sort of. Correct data is stored in x.dat and 0.03333.....dat. Also a combined x and y data file is in gnuplot.gp.

--- src/diffusion1d.cpp
+++ src/diffusion1d.cpp
@@ -8,8 +8,9 @@ void diffusion1d()
     const double dx = 2/(double (nx)-1);
     const int nt = 20;
     const double nu = .3;
-    const double sigma = 2;
+    const double sigma = .2;
     const double dt = (sigma*pow(dx,2.0))/nu;
+    string dir = "step3/";
 
     Eigen::VectorXd u(nx);
 
@@ -21,6 +22,12 @@ void diffusion1d()
     int r = (1/dx+1)-(.5/dx); // The number of rows for starting position.
     // r should always be int, right? ... right? maybe should add a check...
 
+    Eigen::VectorXd x(nx); // This will set our x values to the first column in the matrix
+    x.setLinSpaced(nx,0,2); // linearly spaced values between 0 and 2
+    ofstream writex((dir + "x" + ".dat").c_str(), ios::out | ios::trunc);
+    writex << x << endl;
+    writex.close();
+
     Eigen::VectorXd ic(r); // initial conditions block matrix
     // Needs to be only nt+1, because we don't want to modify our newly created x column
 
@@ -30,7 +37,7 @@ void diffusion1d()
 
     Eigen::VectorXd un(nx);
 
-    string dir = "step3/";
+
 
     mkdir(dir.c_str(),0755); //Make a folder to store results in
 
@@ -39,30 +46,34 @@ void diffusion1d()
     for (int j=0;j<nt;j++) // From column 2 (Eigen is 0-indexed, so really column 3!) to end (of matrix, nt+2)
     {
         un << u;
+
         ostringstream s;
         s << (dt*(j+1)); // Some nasty code to turn our current step into a string
         step = s.str();
 
-        cout << step << endl;
-
         for (int k=1;k<nx-1;k++) // From row 1
         {
-            u(k) = un(k) + ((nu*dt)/(pow(dx,2)))*(un(k+1)-(2*un(k))+un(k-1));
+            u(k) = un(k) + ((nu*dt)/(dx*dx))*(un(k+1)-(2*un(k))+un(k-1));
 
-
-            ofstream writedata((dir + step + ".dat").c_str(), ios::out | ios::trunc);
-            writedata << u << endl;
-            writedata.close();
         }
+
+        ofstream writedata((dir + step + ".dat").c_str(), ios::out | ios::trunc);
+        writedata << u << endl;
+        writedata.close();
+
     }
 
+    ofstream writedata((dir + "gnuplot" + ".dat").c_str(), ios::out | ios::trunc);
+    for (int i=0;i<nx;i++){
+        writedata << x(i) << "\t" << u(i) << endl;
+    }
+
+    writedata.close();
 
     mglGraph gr; // Start mgl graph object that holds all graph stuff
 
-    mglData x;
-    x.Fill(0,2,'x');
     mglData y((dir + step + ".dat").c_str()); // Load one set of data
-    //mglData xdat=y.SubData(0), ydat=y.SubData(21); // Deprecated?
+    mglData z((dir + "x" + ".dat").c_str()); // Load one set of data
 
     gr.SetOrigin(0,0,0);
     gr.Title("Non-Linear 1 Dimension");
@@ -71,7 +82,7 @@ void diffusion1d()
     gr.Legend(); // Add legend to plot
     gr.SetRanges(0,2,0,3);
     gr.Axis(); // Creates ticks, I think
-    gr.Plot(x,y,"r*"); // Same as previous line
+    gr.Plot(z,y,"r*"); // Same as previous line
     gr.WriteGIF("step3/diffusion1d.gif");
 
 }

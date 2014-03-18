879245d86ae7850cc6b9fef6bdfcf1fae49baaf0
Joshua Christopher <jcc242@nau.edu>
2014-03-17 19:01:18 -0700

    More work on step 7

--- src/diffusion2d.cpp
+++ src/diffusion2d.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 void diffusion2d() {
-  double nx = 31;
-  double ny = 31;
-  double nt = 17;
+  double nx = 81;
+  double ny = 81;
+  double nt = 50;
   double nu = .5;
   double dx = 2/(nx-1);
   double dy = 2/(ny-1);
@@ -23,6 +23,7 @@ void diffusion2d() {
   y.setLinSpaced(ny,0,2);
 
   Eigen::ArrayXXd u;
+  u.setOnes(ny,nx);
   u.block(.5/dy, .5/dx, 1/dy+1-.5/dy, 1/dx+1-.5/dx) = Eigen::ArrayXXd::Constant(1/dy+1-.5/dy,1/dx+1-.5/dx,2);
   Eigen::ArrayXXd un;
   un.setOnes(ny,nx);
@@ -30,8 +31,8 @@ void diffusion2d() {
   for(int i=0;i<nt+1;i++) {
     un << u;
     
-    u.block(1,1,ny-1,nx-1) = un.block(1,1,ny-1,nx-1)+nu*pow(dt/dx,2)*(un.block(2,1,ny-2,nx-1)-2*un.block(1,1,ny-1,nx-1)+un(0,1,ny-1,nx-1))+nu*pow(dt/dx,2)*(un.block(1,2,ny-1,ny-2)-2*un(1,1,ny-1,nx-1)+un(1,0,ny-1,nx-1));
-    
+    u.block(1,1,ny-2,nx-2) = un.block(1,1,ny-2,nx-2)+nu*pow(dt/dx,2)*(un.block(2,1,ny-2,nx-2)-2*un.block(1,1,ny-2,nx-2)+un.block(0,1,ny-2,nx-2))+nu*pow(dt/dx,2)*(un.block(1,2,ny-2,ny-2)-2*un.block(1,1,ny-2,nx-2)+un.block(1,0,ny-2,nx-2));
+  
     // Gives boundary condition
     u.block(0,0,1,nx-1) = Eigen::ArrayXXd::Constant(1,nx-1,1);
     u.block(ny-1,0,1,nx-1) = Eigen::ArrayXXd::Constant(1,nx-1,1);
@@ -39,5 +40,9 @@ void diffusion2d() {
     u.block(0,nx-1,ny-1,1) = Eigen::ArrayXXd::Constant(ny-1,1,1);
     
   }
+  
+  ofstream writexdata((dir + "output.dat").c_str(), ios::out | ios::trunc);
+  writexdata << u << endl;
+  writexdata.close();
   
 }

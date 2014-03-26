d3700c4a9ae7026369bf00a144fed9231301ceb6
Joshua Christopher <jcc242@nau.edu>
2014-03-25 21:51:29 -0700

    Further work on step 9

--- src/laplace2d.cpp
+++ src/laplace2d.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 void laplace2d() {
-  double nx = 41;
-  double ny = 41;
-  double nt = 120;
+  double nx = 31;
+  double ny = 31;
   double c = 1;
   double dx = 2/(nx-1);
   double dy = 2/(ny-1);
+  double linorm_target = .01;
+  double linorm = 1;
   
   // Make an output folder
   std::string dir = "step9/";
@@ -29,15 +30,25 @@ void laplace2d() {
   pn.setZero(ny,nx);
   
   //Set Boundary Conditions
-  p.block(0,0,ny-1,1) = Eigen::ArrayXXd::Constant(ny-1,1,0);
-  p.block(0,nx-1,ny-1,1) = y;
-  p.block(0,0,1,nx-1) = p.block(1,0,1,nx-1);
-  p.block(ny-1,0,1,nx-1) = p.block(ny-2,0,1,nx-1);
-  
-  /*for (int i=0;i<nt+1;i++)
+  p.block(0,0,ny,1) = Eigen::ArrayXXd::Constant(ny,1,0);
+  p.block(0,nx-1,ny,1) = y.block(0,0,ny,1);
+  p.block(0,0,1,nx) = p.block(1,0,1,nx);
+  p.block(ny-1,0,1,nx) = p.block(ny-2,0,1,nx);
+
+  while(linorm>linorm_target)
   {
+    pn << p;
+    p.block(1,1,ny-2,nx-2) = (pow(dy,2)*(pn.block(2,1,ny-2,nx-2)+pn.block(0,1,ny-2,nx-2))+pow(dx,2)*(pn.block(1,2,ny-2,nx-2)+pn.block(1,0,ny-2,nx-2)))/(2*(pow(dx,2)+pow(dy,2)));
+    p(0,0) = (pow(dy,2)*(pn(1,0)+pn(ny-1,0))+pow(dx,2)*(pn(0,1)+pn(0,nx-1)))/(2*(pow(dx,2)+pow(dy,2)));
+    p(ny-1,nx-1) = (pow(dy,2)*(pn(0,nx-1)+pn(ny-2,nx-1))+pow(dx,2)*(pn(ny-1,0)+pn(ny-1,nx-2)))/(2*(pow(dx,2)+pow(dy,2)));
     
-  }*/
+    //Set Boundary Conditions
+    p.block(0,0,ny,1) = Eigen::ArrayXXd::Constant(ny,1,0);
+    p.block(0,nx-1,ny,1) = y.block(0,0,ny,1);
+    p.block(0,0,1,nx) = p.block(1,0,1,nx);
+    p.block(ny-1,0,1,nx) = p.block(ny-2,0,1,nx);
+    
+  }
   
   ofstream writexdata((dir + "output.dat").c_str(), ios::out | ios::trunc);
   writexdata << p << endl;
